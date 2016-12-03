db8b358d01a343e52aa99656e2e41f94b2f20217
Jhon <albeosorioz88x@gmail.com>
2016-12-03 18:59:43 -0500

    Creé la actividad de registro

---
+++ app/src/main/java/it/save/tonelist/control/Registrarse.java
@@ -0,0 +1,15 @@
+package it.save.tonelist.control;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import it.save.tonelist.R;
+
+public class Registrarse extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registrarse);
+    }
+}
