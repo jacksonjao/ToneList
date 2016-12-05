ea9792e20834fb53976e8afda3206452c8cf10a4
avillega <villegas.pelaez.andres@gmail.com>
2016-12-05 01:41:05 -0500

    Agregar el adapter Fiestas

--- app/src/main/java/it/save/tonelist/control/MisFiestas.java
+++ app/src/main/java/it/save/tonelist/control/MisFiestas.java
@@ -1,19 +1,31 @@
 package it.save.tonelist.control;
 
-import android.animation.LayoutTransition;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.graphics.Color;
+import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
-import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
-import android.widget.SearchView;
 import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import it.save.tonelist.R;
 
@@ -22,6 +34,15 @@ public class MisFiestas extends AppCompatActivity {
     ImageButton btn_menu;
     RelativeLayout menu;
     DrawerLayout drawerLayout;
+    RecyclerView recyclerView;
+    List<FiestaSimple> fiestas;
+    FiestaAdapter fiestasAdapter;
+    FirebaseUser user;
+    FirebaseDatabase database;
+    DatabaseReference listsReference;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,8 +54,61 @@ public class MisFiestas extends AppCompatActivity {
         menu = (RelativeLayout) findViewById(R.id.dl_menu);
         drawerLayout = (DrawerLayout) findViewById(R.id.activity_lista);
         drawerLayout.setScrimColor(Color.argb(230,0,0,0));
+
+        recyclerView = (RecyclerView) findViewById(R.id.rv_lista);
+        fiestas = new ArrayList<>();
+        fiestasAdapter = new FiestaAdapter(fiestas);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(fiestasAdapter);
         validarMenu();
+
+        database = FirebaseDatabase.getInstance();
+        listsReference = database.getReference().child("lists");
+        cargarUsuario();
     }
+
+
+    public void cargarUsuario() {
+
+
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+        user = mAuth.getCurrentUser();
+        if (user != null) {
+            Log.d("cargarUsuario", "Va a cargar las canciones");
+            cargarCanciones();
+
+        }
+    }
+
+    public void cargarCanciones() {
+        Query cancionesUser = listsReference.orderByChild("creator").equalTo(user.getEmail());
+        cancionesUser.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot child : dataSnapshot.getChildren()) {
+                    FiestaSimple temp = child.getValue(FiestaSimple.class);
+                    temp.code = child.getKey();
+                    Log.d("cargarCanciones", temp.name);
+                    Log.d("cargarCanciones", temp.creationDate + "");
+                    fiestas.add(temp);
+
+
+                }
+                fiestasAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.d("cargarCanciones", "error cargando las canciones");
+                Log.e("cargarCanciones", databaseError.getMessage());
+
+            }
+        });
+
+
+    }
+
+
 
     public void menu(View v) {
         drawerLayout.openDrawer(menu);
