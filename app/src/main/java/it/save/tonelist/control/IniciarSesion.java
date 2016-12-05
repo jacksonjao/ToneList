068fbcd3b7f357377800891b306e976392a7a208
Jhon <albeosorioz88x@gmail.com>
2016-12-04 22:07:57 -0500

    agregué boton, login y registro en firebase

--- app/src/main/java/it/save/tonelist/control/IniciarSesion.java
+++ app/src/main/java/it/save/tonelist/control/IniciarSesion.java
@@ -2,34 +2,88 @@ package it.save.tonelist.control;
 
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 import it.save.tonelist.R;
 
 public class IniciarSesion extends AppCompatActivity {
-    FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
-    DatabaseReference databaseReference = firebaseDatabase.getReference();
+
     EditText et_usuario;
     EditText et_contrasena;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_iniciar_sesion);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d("onAuthStateChanged", "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d("onAuthStateChanged", "onAuthStateChanged:signed_out");
+                }
+                // ...
+            }
+        };
 et_usuario=(EditText)findViewById(R.id.et_correo);
         et_contrasena=(EditText) findViewById(R.id.et_contrasena);
     }
 
-    public void entrar(View v){
-        startActivity(new Intent(getApplicationContext(),Rol.class).setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
 
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
+
+    public void entrar(View v){
+        mAuth.signInWithEmailAndPassword(et_usuario.getText().toString(),et_contrasena.getText().toString())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d("onComplete", "signInWithEmail:onComplete:" + task.isSuccessful());
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Log.w("onComplete", "signInWithEmail", task.getException());
+                            Toast.makeText(getApplicationContext(), "Usuario o contraseña inválidos",
+                                    Toast.LENGTH_SHORT).show();
+                        }else{
+                            startActivity(new Intent(getApplicationContext(),Rol.class).setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
+                        }
+                    }
+                });
     }
 
     public void registrarse(View v){
--- app/src/main/java/it/save/tonelist/control/Registrarse.java
+++ app/src/main/java/it/save/tonelist/control/Registrarse.java
@@ -1,18 +1,75 @@
 package it.save.tonelist.control;
 
 import android.content.pm.ActivityInfo;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import it.save.tonelist.R;
 
 public class Registrarse extends AppCompatActivity {
-
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    EditText et_usuario;
+    EditText et_contrasena;
+    EditText et_confirmar_contrasena;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registrarse);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        mAuth = FirebaseAuth.getInstance();
 
+        et_usuario=(EditText)findViewById(R.id.et_correo);
+        et_contrasena=(EditText) findViewById(R.id.et_contrasena);
+        et_confirmar_contrasena=(EditText) findViewById(R.id.et_confirmar_contrasena);
+    }
+
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
+    public void registrarse(View v){
+        mAuth.createUserWithEmailAndPassword(et_usuario.getText().toString(), et_contrasena.getText().toString())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d("onComplete", "createUserWithEmail:onComplete:" + task.isSuccessful());
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Toast.makeText(getApplicationContext(), "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }else{
+                            Toast.makeText(getApplicationContext(), "Te has registrado satisfactoriamente",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                        // ...
+                    }
+                });
     }
 }
