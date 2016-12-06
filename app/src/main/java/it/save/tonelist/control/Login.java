4aa8458066f918c4eeed3148a6e545951c3a9e8e
Jhon <albeosorioz88x@gmail.com>
2016-12-05 20:58:01 -0500

    Cargo imagen

--- app/src/main/java/it/save/tonelist/control/Login.java
+++ app/src/main/java/it/save/tonelist/control/Login.java
@@ -1,23 +1,23 @@
 package it.save.tonelist.control;
 
 import android.content.Intent;
-import android.content.pm.ActivityInfo;
-import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Toast;
+        import android.content.pm.ActivityInfo;
+        import android.support.annotation.NonNull;
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+        import android.util.Log;
+        import android.view.View;
+        import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
+        import com.google.android.gms.tasks.OnCompleteListener;
+        import com.google.android.gms.tasks.Task;
+        import com.google.firebase.auth.AuthResult;
+        import com.google.firebase.auth.FirebaseAuth;
+        import com.google.firebase.auth.FirebaseUser;
+
 
 import it.save.tonelist.R;
+
 
 public class Login extends AppCompatActivity {
     private FirebaseAuth mAuth;
@@ -46,7 +46,7 @@ public class Login extends AppCompatActivity {
 
     public void iniciarSesion(View v){
 
-startActivity(new Intent(getApplicationContext(),IniciarSesion.class).setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
+        startActivity(new Intent(getApplicationContext(),IniciarSesion.class).setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
 
     }
 
